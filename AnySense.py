2e9dcdca8cc379fafddd13da3a8484c79ce99d3f
Ling-Jyh Chen <cclljj@gmail.com>
2017-09-04 15:53:28 +0000

    Add the support of the Airsense S8 CO2 sensor

--- AnySense.py
+++ AnySense.py
@@ -6,11 +6,17 @@ import string
 import re
 from threading import Timer
 from datetime import datetime
+from multiprocessing import Queue
 
-import pm_g5t as air_sensor
+import pm_g5 as pm_sensor
 import th_htu21d as tmp_sensor
 import light_bh1750fvi as light_sensor
-from multiprocessing import Queue
+import co2_s8 as gas_sensor
+
+Sense_PM = 1
+Sense_Tmp = 1
+Sense_Light = 1
+Sense_Gas = 1
 
 MQTT_broker = 'gpssensor.ddns.net'
 MQTT_port = 1883
@@ -28,6 +34,7 @@ fields ={ 	"Tmp"	:	"s_t0",
 		"PM2.5"	:	"s_d0",
 		"PM10"	:	"s_d1",
 		"Lux"	:	"s_l0",
+		"CO2"	:	"s_g8",
 	}
 values = {	"app"		:	APP_ID,
 		"device_id"	:	DEVICE_ID,
@@ -45,7 +52,8 @@ values = {	"app"		:	APP_ID,
 	}
 
 def upload_data():
-	Timer(60,upload_data,()).start()
+	# setup a timer, in seconds, to trigger the next upload_data
+	Timer(10,upload_data,()).start()
 	timestamp = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
 	pairs = timestamp.split(" ")
 	values["device_id"] = DEVICE_ID
@@ -53,42 +61,46 @@ def upload_data():
 	values["time"] = pairs[1]
 	msg = ""
 	for item in values:
-		#print item + " : " + str(values[item])
-		#if item == "device_id":
-		#	msg = msg + "|" + item + "=\"" + str(values[item]) + "\""
 		if num_re_pattern.match(str(values[item])):
 			msg = msg + "|" + item + "=" + str(values[item]) + ""
 		else:
 			tq = values[item]
 			tq = tq.replace('"','')
 			msg = msg + "|" + item + "=" + tq 
-	MQTT = mqtt.mqtt(MQTT_broker,MQTT_port,MQTT_topic + "/" + DEVICE_ID)
-	MQTT.pub(msg)
+	#MQTT = mqtt.mqtt(MQTT_broker,MQTT_port,MQTT_topic + "/" + DEVICE_ID)
+	#MQTT.pub(msg)
 	print msg
 	
 
 if __name__ == '__main__':
 
-
-	air_data = '1'
-	tmp_data = '2'
-	light_data = '3'
-
-	air_q = Queue()
+	pm_q = Queue()
 	tmp_q = Queue()
 	light_q = Queue()
+	gas_q = Queue()
 
-	air = air_sensor.sensor(air_q)
-	tmp = tmp_sensor.sensor(tmp_q)
-	light = light_sensor.sensor(light_q)
+	if Sense_PM==1:
+		pm_data = '1'
+		pm = pm_sensor.sensor(pm_q)
+		pm.start()
 
-	air.start()
-	tmp.start()
-	light.start()
+	if Sense_Tmp==1:
+		tmp_data = '2'
+		tmp = tmp_sensor.sensor(tmp_q)
+		tmp.start()
+		tmp_data = {'Tmp':0.0, 'RH':0}
+
+	if Sense_Light==1:
+		light_data = '3'
+		light = light_sensor.sensor(light_q)
+		light.start()
+
+	if Sense_Gas==1:
+		gas_data = '4'
+		gas = gas_sensor.sensor(gas_q)
+		gas.start()
 
 	pat = 'PM1.0={pm1_0},PM2.5={pm2_5},PM10={pm10},Tmp={tmp:5.3f},RH={rh},Lux={lux}'
-
-	tmp_data = {'Tmp':0.0, 'RH':0}
 
 	mac = str(':'.join(['{:02x}'.format((uuid.getnode() >> i) & 0xff) for i in range(0,8*6,8)][::-1])).upper()
 	DEVICE_ID = mac.replace(':','')
@@ -97,27 +109,36 @@ if __name__ == '__main__':
 	sn = 1
 	while True:
 		flag = 0
-		if not air_q.empty():
-			air_data = air_q.get()
-			for item in air_data:
+		if Sense_PM==1 and not pm_q.empty():
+			pm_data = pm_q.get()
+			for item in pm_data:
 				if item in fields:
-					values[fields[item]] = air_data[item]
+					values[fields[item]] = pm_data[item]
 				else:
-					values[item] = air_data[item]
-		if not light_q.empty():
+					values[item] = pm_data[item]
+
+		if Sense_Tmp==1 and not tmp_q.empty():
+			tmp_data = tmp_q.get()
+                        for item in tmp_data:                                                                 
+                                if item in fields:                                                                
+                                        values[fields[item]] = tmp_data[item]                                     
+                                else:                                                                             
+                                        values[item] = tmp_data[item]
+
+		if Sense_Light==1 and not light_q.empty():
 			light_data = light_q.get()
                         for item in light_data:                                                                 
                                 if item in fields:                                                                
                                         values[fields[item]] = light_data[item]                                     
                                 else:                                                                             
                                         values[item] = light_data[item]                                             
-		if not tmp_q.empty():
-			tmp_data = tmp_q.get()
-                        for item in tmp_data:                                                                 
-                                if item in fields:                                                                
-                                        values[fields[item]] = tmp_data[item]                                     
-                                else:                                                                             
-                                        values[item] = tmp_data[item]                                             
 
-		time.sleep(5)
+		if Sense_Gas==1 and not gas_q.empty():
+			gas_data = gas_q.get()
+                        for item in gas_data:                                                                 
+                                if item in fields:                                                                
+                                        values[fields[item]] = gas_data[item]                                     
+                                else:                                                                             
+                                        values[item] = gas_data[item]                                             
+
 
---
+++ co2_s8.py
@@ -0,0 +1,72 @@
+import mraa
+import time
+
+from multiprocessing import Queue,Process
+
+import move_avge
+
+CO2_BYTE = 9
+NUM_INCOME_BYTE = 13
+S8_message = b"\xFE\x04\x00\x00\x00\x04\xE5\xC6"
+
+class sensor(Process):
+	def __init__(self, q):
+		Process.__init__(self)
+		self.q = q
+
+		self.u=mraa.Uart(1)
+		self.u.setBaudRate(9600)
+		self.u.setMode(8, mraa.UART_PARITY_NONE, 1)
+		self.u.setFlowcontrol(False, False)
+
+		self.co2_avg = move_avge.move_avg(1)		
+
+	def data_log(self, dstr):
+		bytedata = bytearray(dstr)
+		if self.checksum(dstr) is True:
+			CO2 = bytedata[CO2_BYTE]*256 + bytedata[CO2_BYTE+1]
+			self.co2_avg.add(CO2)
+		else:
+			return
+
+
+	def checksum(self, dstr):
+		return True
+
+
+	def get_data(self):
+		CO2 = self.co2_avg.get()
+		ret = {	'CO2': CO2
+			}
+		return ret
+
+	def run(self):
+		count = 0
+
+		while True:
+			self.u.writeStr(S8_message)
+			self.u.flush()
+			if self.u.dataAvailable():
+				time.sleep(0.05)
+				getstr = self.u.readStr(NUM_INCOME_BYTE)
+
+				if len(getstr) == NUM_INCOME_BYTE:
+					self.data_log(getstr)
+
+					if count == 0:
+						g = self.get_data()
+						self.q.put(g)
+					count = (count+1)%10
+					
+
+
+if __name__ == '__main__':
+
+	q = Queue()
+	p = sensor(q)
+	p.start()
+
+
+	while True:
+		print('co2: '+ str(q.get()))
+
