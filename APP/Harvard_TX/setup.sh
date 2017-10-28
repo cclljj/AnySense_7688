b5d20cba0e83ec48d1e2c894219bd901f98d7371
Ling-Jyh Chen <cclljj@gmail.com>
2017-10-28 09:27:42 +0000

    change setup.sh

--- APP/Harvard_TX/setup.sh
+++ APP/Harvard_TX/setup.sh
@@ -2,7 +2,14 @@ cp $PWD/AnySense /etc/init.d
 chmod 755 /etc/init.d/AnySense
 /etc/init.d/AnySense enable
 
+
+opkg update
+opkg install block-mount kmod-fs-ext4 kmod-usb-storage-extras e2fsprogs fdisk
+mkfs.ext4 /dev/mmcblk0p1
+
+
 block detect > /etc/config/fstab
 echo "	option	enabled	'1'" >> /etc/config/fstab
 
 reboot
+
