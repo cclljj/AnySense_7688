589c7075e235e72fb76d7ac23a23d6d2c00744c5
Ling-Jyh Chen <cclljj@gmail.com>
2017-10-31 17:05:49 +0000

    rtc support

--- AnySense_RTC.py
+++ AnySense_RTC.py
@@ -7,6 +7,8 @@ import os
 from threading import Timer
 from datetime import datetime
 
+TimeURL = "https://pm25.lass-net.org/util/timestamp.php"
+
 def rtc_set_time():
 	print "Ya 1"
 
@@ -14,6 +16,16 @@ def rtc_get_time():
 	print "Ya 2"
 
 def ntp_is_running():
+	command = "wget -O /tmp/time1 " + TimeURL
+	os.system(command)
+	fh = open("/tmp/time1","r")
+	time1 = fh.read()
+
+	time2 = datetime.utcnow()
+	time2 = time2.strftime("%Y-%m-%d %H:%M:%S")
+
+	print time1
+	print time2
 	print "Ya"
 	return 0
 
