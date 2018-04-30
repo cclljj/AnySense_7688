1440646e0fdfff073428ea06aab0d50e1c6ded18
SCWhite <sean3381212@gmail.com>
2018-05-01 03:04:20 +0800

    Add "Prototype" version
    Prototype version:
    lower start time/log time
    add a restart flag

---
+++ APP/Harvard_TX_Prototype/README.md
@@ -0,0 +1,34 @@
+# Harvard_TX Project
+
+This is the main page for the Harvard_TX project. The development of this project is based on the AnySense_7688 project. However, we are sorry that we cannot release further information because most of the details are remaining confidental. We will make that public once we are authorized to do so.
+
+## Hardware
+
+* MediaTek LinkIt Smart 7688
+* MediaTek LinkIt Smart 7688 Breakout v2.0
+* TVOC sensor: Sensirion SGP30
+* T/H/PM sensor: Plantower PMS5003T
+* RTC: DS3231
+* 0.96" LCD
+* SD card
+
+## How to re-install the codes?
+
+Please use the designated account/password to login the system, and remove the existing project source codes:
+
+## How to install the codes (for new LinkIt Smart 7688 boards)?
+
+Please login you 7688 board, and change the working directory to /root. Then, please follow the following steps:
+
+1. Use the coommand to get the latest version of the codes: 
+   ```
+   git clone https://github.com/cclljj/AnySense_7688
+   ```
+2. update the values of GPS_LAT and GPS_LON with your GPS coordinates in the file /root/AnySense_7688/APP_Harvard_TX_config.py
+
+3. Run the setup script:
+   ```
+   /root/AnySense_7688/APP/Harvard_TX/setup.sh KEY
+   ```
+
+Then, the script will install and configure everything. The system will reboot after the installation is done, and hopefully the system will start to upload data to the backend database.
---
+++ APP_Harvard_IAQ_Prototype_config.py
@@ -0,0 +1,101 @@
+import pm_g3 as pm_sensor
+import th_htu21d as tmp_sensor
+import light_tcs34725 as light_sensor
+#import gas_tvoc_sgp30 as gas_sensor
+import gas_co2_s8 as gas_sensor
+import pyupm_i2clcd as upmLCD
+
+
+Version = "0.1.2"
+
+Sense_PM = 1                          
+Sense_Tmp = 1
+Sense_Light = 1
+Sense_Gas = 1
+Use_RTC_DS3231 = 1
+
+GPS_LAT = 25.1933
+GPS_LON = 121.7870
+APP_ID = "Harvard_IAQ"
+DEVICE = "LinkIt_Smart_7688"
+DEVICE_ID = "DEVICE_ID1234"
+DEVICE_IP = ''
+
+Interval_LCD = 5
+
+Reboot_Time = 3600
+
+Restful_URL = "https://data.lass-net.org/Upload/MAPS-secure.php?"
+Restful_interval = 60			# 60 seconds
+
+SecureKey = "NoKey"
+
+FS_SD = "/mnt/mmcblk0p1"
+
+#################################
+# don't make any changes in the following codes
+
+import uuid
+import re
+import os
+from multiprocessing import Queue
+
+float_re_pattern = re.compile("^-?\d+\.\d+$")                                                                                               
+num_re_pattern = re.compile("^-?\d+\.\d+$|^-?\d+$")
+
+mac = open('/sys/class/net/eth0/address').readline().upper().strip()
+DEVICE_ID = mac.replace(':','') 
+
+f = os.popen('ifconfig eth0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
+DEVICE_IP=f.read()
+if(DEVICE_IP == ''):
+        f = os.popen('ifconfig apcli0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
+        DEVICE_IP=f.read()    
+
+
+if Use_RTC_DS3231 == 1:
+	import mraa
+	import hmac
+	import hashlib
+	import base64
+	import urllib
+
+	DS3231_I2C_ADDR = 0x68
+	rtc = mraa.I2c(0)
+	rtc.address(DS3231_I2C_ADDR)
+	SecureKey = chr(rtc.readReg(0x07)) + chr(rtc.readReg(0x08)) + chr(rtc.readReg(0x09)) + chr(rtc.readReg(0x0A)) + chr(rtc.readReg(0x0B)) + chr(rtc.readReg(0x0C)) + chr(rtc.readReg(0x0D))
+	SecureKey = urllib.quote_plus(SecureKey)
+	print "SecureKey = " , SecureKey
+
+pm_q = Queue(maxsize=5)                                                                                                                     
+tmp_q = Queue(maxsize=5)                                                                                                                     
+light_q = Queue(maxsize=5)                                                                                                                   
+gas_q = Queue(maxsize=5)  
+tvoc_q = Queue(maxsize=5)
+
+fields ={       "Tmp"   :       "s_t0",           
+                "RH"    :       "s_h0",           
+                "PM1.0" :       "s_d2",           
+                "PM2.5" :       "s_d0",           
+                "PM10"  :       "s_d1",              
+                "Lux"   :       "s_l0",
+		"RGB_R"	:	"s_lr",
+		"RGB_G"	:	"s_lg",
+		"RGB_B"	:	"s_lb",
+		"RGB_C"	:	"s_lc",
+                "CO2"   :       "s_g8e",              
+		"TVOC"	:	"s_gg",
+        }                                            
+values = {      "app"           :       APP_ID,      
+                "device_id"     :       DEVICE_ID,                  
+                "device"        :       DEVICE,                     
+                "ver_format"    :       3,                        
+                "fmt_opt"       :       0,                        
+                "gps_lat"       :       GPS_LAT,                    
+                "gps_lon"       :       GPS_LON,                    
+                "FAKE_GPS"      :       1,                        
+                "gps_fix"       :       1,                        
+                "gps_num"       :       100,                      
+                "date"          :       "1900-01-01",                        
+                "time"          :       "00:00:00",                          
+        }                       
