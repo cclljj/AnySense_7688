ec8fdbab9b8288f6fcec581b2ef03cf296b817f0
SCWhite <sean3381212@gmail.com>
2019-01-09 15:44:21 +0800

    add a temporarily project

---
+++ APP/Temp_Project/setup.sh
@@ -0,0 +1,17 @@
+cp /root/AnySense_7688/APP/Harvard_IAQ/AnySense /etc/init.d
+chmod 755 /etc/init.d/AnySense
+/etc/init.d/AnySense enable
+
+
+opkg update
+opkg install wget block-mount kmod-fs-ext4 kmod-usb-storage-extras e2fsprogs fdisk
+mkfs.ext4 /dev/mmcblk0p1
+
+block detect > /etc/config/fstab
+echo "	option	enabled	'1'" >> /etc/config/fstab
+
+python /root/AnySense_7688/AnySense_RTC.py -d 0 -k $1
+crontab /root/AnySense_7688/APP/Harvard_IAQ/RTC_cron
+
+reboot
+
