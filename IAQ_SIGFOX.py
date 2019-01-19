0098bd7c5b92e6cf8feb9baad4b9b6c3a94063ed
SCWhite <sean3381212@gmail.com>
2019-01-19 11:52:03 +0800

    error fix

--- IAQ_SIGFOX.py
+++ IAQ_SIGFOX.py
@@ -16,17 +16,19 @@ values = Conf.values
 #for sigfox 
 try:
     port = serial.Serial("/dev/ttyUSB0",baudrate=9600, timeout=3.0)
-    port.write("AT$I=10\r\n")
-    time.sleep(0.5)
-    rcv = readlineCR(port)
-    sigfox_id= rcv[-6:]
-    print "==============ID==============="
-    print sigfox_id
-    print "==============ID==============="
     sigfox_flag = " "
 except:
     print "there is no sigfox on the board!!!"
     sigfox_flag = "!"
+
+
+port.write("AT$I=10\r\n")
+time.sleep(0.5)
+rcv = readlineCR(port)
+sigfox_id= rcv[-6:]
+print "==============ID==============="
+print sigfox_id
+print "==============ID==============="
 #
 
 def readlineCR(port):
